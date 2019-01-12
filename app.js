7ead03539d023ae0f09aeb2cbd571f2b3c45e939
Pitsanujiw <pitsanujill12@gmail.com>
2019-01-12 16:17:28 +0700

    key

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function sendText (sender, text) {
       headers: {
         'Content-Type': 'application/json',
         Authorization:
-          'Bearer key Apit1nBzbDCnZ5aX/kBpRZHqxP3kn3vm+HJFSs0ekFMD9799yPv/p9yeNq4fQ9qMDc5htjnYr7RW7SMvbu4OwH3Tzrz78L4rocNHtvYPvUWm3Rp5BNi+cFxXN/IPXJRn1htdivdiHGJRgxIUiHf3GLeNAdB04t89/1O/w1cDnyilFU='
+          'Bearer t1nBzbDCnZ5aX/kBpRZHqxP3kn3vm+HJFSs0ekFMD9799yPv/p9yeNq4fQ9qMDc5htjnYr7RW7SMvbu4OwH3Tzrz78L4rocNHtvYPvUWm3Rp5BNi+cFxXN/IPXJRn1htdivdiHGJRgxIUiHf3GLeNAdB04t89/1O/w1cDnyilFU='
       },
       url: 'https://api.line.me/v2/bot/message/push',
       method: 'POST',
